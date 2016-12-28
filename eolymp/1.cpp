02efee8b3da8606e539267855767488d0f58c857
Vitaliy Yeskin <vitaliy.yeskin@gmail.com>
2016-12-28 23:28:06 +0100

    added "development" configuration resolved EOLYMP#1

--- eolymp/1.cpp
+++ eolymp/1.cpp
@@ -1,15 +1,18 @@
 //
-//  main.cpp
-//  eolymp
+//  1.cpp
+//  development
 //
 //  Created by vitaliy on 12/28/16.
 //  Copyright © 2016 YesApp. All rights reserved.
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int number;
+    cin >> number;
+    cout << number/10 << " " << number%10 << endl;
+    
     return 0;
 }
